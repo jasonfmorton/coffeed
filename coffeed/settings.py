f643ac2f4787b71fbbdd1b401db4132c1422fe79
jasmorton <jason.morton@davita.com>
2015-11-16 11:16:52 -0700

    Adding for Heroku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -84,6 +84,16 @@ DATABASES = {
     }
 }
 
+# Added Lesson 8
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -102,6 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+#create path that will serve up static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
