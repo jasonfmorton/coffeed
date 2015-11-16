bf5e32490efc9070f2afbbcda966c957e72b3930
jasmorton <jason.morton@davita.com>
2015-11-16 12:16:45 -0700

    Adding for Heroku

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -18,12 +18,11 @@ from dj_static import Cling
 application = Cling(get_wsgi_application())
 
 
-'''
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
-application = get_wsgi_application()
-'''
+"""
